ecf95c78cc811bd121180b6dd4a166553f4947d6
Chris Pressey <cpressey@catseye.tc>
2014-11-24 08:46:37 +0000

    Improve smarts: don't re-use samples, interpolate patterns.

--- gen_all.sh
+++ gen_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-./smarts.py > opera.json
+./smarts.py > opera.json || exit 1
 for SCENE in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
   ./carpet.py opera.json $SCENE --minutes=8 --output-name="Scene_$SCENE"
 done
--- smarts.py
+++ smarts.py
@@ -11,6 +11,10 @@ def random_pop(l):
     return l.pop(d)
 
 
+TYPES = (
+    'choral', 'orch', 'speech', 'piano', 'short'
+)
+
 PATTERNS = [
     'X',
     'X X',
@@ -21,20 +25,44 @@ PATTERNS = [
 ]
 
 
-def make_scene(available):
-    keys = available.keys()
+def expand_pattern(base, expansion):
+    new = ''
+    for char in base:
+        if char == ' ':
+            new += ' ' * len(expansion)
+        else:
+            new += expansion
+    return new
+
+
+def make_scene(sample_library):
 
     layers = []
     used_patterns = set()
 
     for x in xrange(0, 4):
-        type_ = random.choice(keys)
+        if x == 0:
+            type_ = 'choral'
+        else:
+            type_ = random.choice(TYPES)
+            while not sample_library[type_]:
+                type_ = random.choice(TYPES)
+
         pattern = None
         while pattern is None or pattern in used_patterns:
             pattern = random.choice(PATTERNS)
+            if random.randint(1, 2) == 2:
+                pattern = expand_pattern(pattern, random.choice(PATTERNS))
+
+        try:
+            sample = random_pop(sample_library[type_])
+        except ValueError:
+            print "No more samples of type %s available!!!" % type_
+            raise
+
         layers.append({
             'type': type_,
-            'sample': random.choice(available[type_]),
+            'sample': sample,
             'pattern': pattern
         })
         used_patterns.add(pattern)
@@ -50,15 +78,18 @@ def main(argv):
     (options, args) = optparser.parse_args(argv[1:])
 
     master_dir = options.master_dir
-    types = ('choral', 'orch', 'speech', 'piano', 'short')
 
-    available = {}
-    for t in types:
-        available[t] = os.listdir(os.path.join(master_dir, t))
+    sample_library = {}  # type -> list of filename
+
+    for type_ in TYPES:
+        type_dir = os.path.join(master_dir, type_)
+        for filename in os.listdir(type_dir):
+            full_filename = os.path.join(type_dir, filename)
+            sample_library.setdefault(type_, []).append(full_filename)
 
     scenes = []
     for scene_num in xrange(1, 16):
-        scene = make_scene(available)
+        scene = make_scene(sample_library)  # will delete from sample_library
         scenes.append(scene)
 
     print json.dumps(scenes)
